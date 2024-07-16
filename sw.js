7e6aaca9207fa4df011825b28a2d0f959a1dda3e
ThUndertaker <lf2014462166@qq.com>
2024-07-16 23:02:58 +0800

    Site updated: 2024-07-16 23:02:58

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/ThUndertaker.github.io/css/style.css",
   "/ThUndertaker.github.io/js/script.js",
 ];
-const VERSION = "1721137442736";
+const VERSION = "1721142175756";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
